64210d49b03e021ac340284431f2b985522ed92e
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-08-05 09:39:14 -0400

    update

--- myapp/app/api/register/route.js
+++ myapp/app/api/register/route.js
@@ -1,26 +1,25 @@
-import bcrypt from "bcrypt"
-import { PrismaClient } from "@prisma/client"
-import { NextResponse } from "next/server"
+// route.js
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+import prisma from "../../../libs/prismadb";
 
-import prisma from "../../../libs/prismadb"
+export async function POST(request, response) {
+  try {
+    const { email, username, password } = request.body;
 
-export async function POST (request, response){
-    try {
-        const { email, username,  password } = req.body;
-    
-        const hashedPassword = await bcrypt.hash(password, 12);
-    
-        const user = await prisma.user.create({
-          data: {
-            email,
-            username,
-            hashedPassword,
-          }
-        });
-    
-        return NextResponse.json(user)
-      } catch (error) {
-        console.log(error);
-        return NextResponse.json(400);
-      }
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        username,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(400);
+  }
 }
